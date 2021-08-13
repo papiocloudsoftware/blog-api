5a79da35e5ee092aab51a2beea34b5cdff466123
Max Schenkelberg <max@papiocloud.com>
2021-08-13 12:19:47 -0500

    using correct variable for domain

--- packages/cdk-app/bin/app.ts
+++ packages/cdk-app/bin/app.ts
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
 
 import { App } from "@aws-cdk/core";
-import * as path from "path";
 
 import { BlogApiApp } from "../lib/BlogApiApp";
 
 const app = new App();
-const host = app.node.tryGetContext("host") ?? path.basename(process.cwd());
+const host = app.node.tryGetContext("host") ?? "blog";
 const domain = app.node.tryGetContext("domain") ?? "papiocloud.com";
 let domainName: string | undefined = undefined;
 if (host && domain) {
-  domainName = `${host}${BlogApiApp.DASH_APP_SUFFIX}.${domainName}`;
+  domainName = `${host}${BlogApiApp.DASH_APP_SUFFIX}.${domain}`;
 }
 BlogApiApp.populate(app, { domainName });
 app.synth();
